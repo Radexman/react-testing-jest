7d096a88f2cbe7a049a100974e488b36e848b6f5
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-21 11:08:11 +0200

    TDD Showcase

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,8 +1,14 @@
-import Greet from './Greet';
 import { render, screen } from '@testing-library/react';
+import Greet from './Greet';
 
 test('Greet renders correctly', () => {
 	render(<Greet />);
-	const textElement = screen.getByText(/hello/i);
+	const textElement = screen.getByText('Hello');
+	expect(textElement).toBeInTheDocument();
+});
+
+test('Greet renders with a name', () => {
+	render(<Greet name='Emilia' />);
+	const textElement = screen.getByText('Hello Emilia');
 	expect(textElement).toBeInTheDocument();
 });
