c9233f2690c3ee0fc76a046a62892b3867ae401e
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-10-30 21:24:18 +0100

    getByLabelText Query Method

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -20,6 +20,11 @@ describe('Application', () => {
 		});
 		expect(nameElement).toBeInTheDocument();
 
+		const nameElemntTwo = screen.getByLabelText('Name', {
+			selector: 'input',
+		});
+		expect(nameElemntTwo).toBeInTheDocument();
+
 		const bioElement = screen.getByRole('textbox', {
 			name: 'Bio',
 		});
@@ -30,6 +35,9 @@ describe('Application', () => {
 
 		const termsElement = screen.getByRole('checkbox');
 		expect(termsElement).toBeInTheDocument();
+
+		const termsElementTwo = screen.getByLabelText('I agree to the terms and conditions');
+		expect(termsElementTwo).toBeInTheDocument();
 
 		const submitElement = screen.getByRole('button');
 		expect(submitElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -19,7 +19,7 @@ const Application = () => {
 					></textarea>
 				</div>
 				<div>
-					<label htmlFor='job-location'>Job location</label>
+					<label htmlFor='job-location'>Name</label>
 					<select id='job-location'>
 						<option value=''>Select a country</option>
 						<option value='US'>United States</option>
