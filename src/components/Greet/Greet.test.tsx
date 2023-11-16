30f5dbec5a954a85d8921797f040b0b8753c6f03
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-16 19:36:59 +0100

    Tests Reminder

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-describe('Greet component', () => {
-	it('should render hello', () => {
+describe('Greet renders correctly', () => {
+	test('it should render guest in no prop passes', () => {
 		render(<Greet />);
-		const textElement = screen.getByText(/hello/i);
+		const textElement = screen.getByText('Hello guest');
+		expect(textElement).toBeInTheDocument();
+	});
+
+	test('it should render prop', () => {
+		render(<Greet name='Emilia' />);
+		const textElement = screen.getByText('Hello Emilia');
 		expect(textElement).toBeInTheDocument();
 	});
 });
